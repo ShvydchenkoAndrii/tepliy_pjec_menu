a7da87d1db867c8eb4d8c7faae3f87ed091507f3
unknown <shvydchenkoandriy@gmail.com>
2023-03-30 21:52:39 +0300

    little changes

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -1,8 +1,10 @@
 export default function Footer() {
   return (
     <div className="flex flex-col items-center m-8">
-      <h2>© Кав&apos;ярня-музей &apos;&apos;Теплий п&apos;єц&apos;&apos; · 2015</h2>
       <h2>☏ +380987321414 </h2>
+      <h2>
+        © Кав&apos;ярня-музей &apos;&apos;Теплий п&apos;єц&apos;&apos; · 2015
+      </h2>
     </div>
   );
 }
