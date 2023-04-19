7ec3d821920df1505d49e1a7cafb28a75ca1b7c1
unknown <shvydchenkoandriy@gmail.com>
2023-04-19 19:13:25 +0300

    added cocktails and cold drinks pict

--- components/Cocktails/Cocktails.js
+++ components/Cocktails/Cocktails.js
@@ -7,79 +7,91 @@ export default function Cocktails() {
       portion: "200мл",
       price: "₴132",
       description: "aperol, prosecco, orange",
-      image: "/lvivske-svitle.png",
+      image: "aperol.png",
     },
     {
       name: "Old Fashioned",
       portion: "??",
       price: "₴140",
       description: "бурбон, бітер Angostura, апельсинова цедра",
+      image: "old-fashioned.png",
     },
     {
       name: "Negroni",
       portion: "120мл",
       price: "₴160",
       description: "джин, вермут, бітер, апельсинова цедра",
+      image: "negroni.png",
     },
     {
       name: "Vesper Martini",
       portion: "100мл",
       price: "₴112",
       description: "сухий вермут, джин",
+      image: "vesper-martini.png",
     },
     {
       name: "Moxito",
       portion: "??",
       price: "??",
       description: "",
+      image: "moxito.png",
     },
     {
       name: "Moxito N/A",
       portion: "??",
       price: "??",
       description: "безалкогольне",
+      image: "moxito.png",
     },
     {
       name: "Глінтвейн",
       portion: "??",
       price: "??",
       description: "сезонний",
+      image: "glintwein.png",
     },
     {
       name: "Сангрія",
       portion: "??",
       price: "??",
       description: "сезонний",
+      image: "sangrija.png",
     },
     {
       name: "Медівка",
-      portion: "??",
+      portion: "50мл",
       price: "₴25",
       description: "",
+      image: "medivka.png",
     },
     {
       name: "Калганівка",
-      portion: "??",
+      portion: "50мл",
       price: "₴30",
       description: "",
+      image: "medivka.png",
     },
     {
       name: "Шипшинівка",
-      portion: "??",
+      portion: "50мл",
       price: "₴28",
       description: "",
+      image: "medivka.png",
     },
     {
       name: "Вишнівка",
-      portion: "??",
+      portion: "50мл",
       price: "₴27",
       description: "",
+      image: "vishnivka.png",
     },
     {
       name: 'СЕТ НАЛИВОК "Апостол"',
       portion: "12шт",
       price: "₴130",
       description: "",
+      image: "apostol.png",
     },
   ];
 
@@ -88,14 +100,14 @@ export default function Cocktails() {
       <div className="partHead">
         <Image
           src={pjecMenuIco}
-          width={90}
+          width={30}
           className="partHeadIco"
           alt="міні логотип теплого п'єца для меню "
         ></Image>
         <h1 className="partName">КОКТЕЙЛІ</h1>
         <Image
           src={pjecMenuIco}
-          width={90}
+          width={30}
           className="partHeadIco"
           alt="міні логотип теплого п'єца для меню "
         ></Image>
@@ -103,12 +115,12 @@ export default function Cocktails() {
       {cocktails.map((item) => {
         return (
           <div key={item.name} className="partItem">
-              <img src={item.image} width={100}></img>
-              <h2 className="itemName">{item.name}</h2>
-              <p className="itemDescrip">{item.description}</p>
-              <p className="itemPortion">{item.portion}</p>
-              <p className="itemPrice">{item.price}</p>
-            </div>
+            <img src={item.image} width={100}></img>
+            <h2 className="itemName">{item.name}</h2>
+            <p className="itemDescrip">{item.description}</p>
+            <p className="itemPortion">{item.portion}</p>
+            <p className="itemPrice">{item.price}</p>
+          </div>
         );
       })}
       <div className="blackUnderlline"></div>
--- components/Cold drinks/Cold_drinks.js
+++ components/Cold drinks/Cold_drinks.js
@@ -7,54 +7,63 @@ export default function ColdDrinks() {
       portion: "1л",
       price: "₴38",
       description: "",
+      image: "uzvar.png",
     },
     {
       name: "Pepsi",
       portion: "0,33л",
       price: "₴50",
       description: "скло",
+      image: "pepsi.png",
     },
     {
       name: "Coca Cola",
       portion: "0,33л",
       price: "₴50",
       description: "скло",
+      image: "coca-cola.png",
     },
     {
       name: "Соки та нектари",
       portion: "0,2л",
       price: "₴26",
       description: "в асортименті",
+      image: "juice.png",
     },
     {
       name: "Поляна Квасова",
       portion: "0,5л",
       price: "₴54",
       description: "",
+      image: "poljana.png",
     },
     {
       name: "Моршинська",
       portion: "0,33л 0,5л 0,75л",
       price: "₴49 ₴37  ₴41",
       description: "в асортименті",
+      image: "morshinska.png",
     },
     {
       name: "Квас",
       portion: "0,5л",
       price: "",
       description: "сезонний",
+      image: "kvas.png",
     },
     {
       name: "Фруктова вода",
       portion: "0,5л",
       price: "₴49",
       description: "дюшес, сітро",
+      image: "fruit-water.png",
     },
     {
       name: "Молочний коктейль",
       portion: "0,2л",
       price: "",
       description: "сезонний",
+      image: "milk-cocktail.png",
     },
   ];
 
@@ -63,14 +72,14 @@ export default function ColdDrinks() {
       <div className="partHead">
         <Image
           src={pjecMenuIco}
-          width={90}
+          width={30}
           className="partHeadIco"
           alt="міні логотип теплого п'єца для меню "
         ></Image>
         <h1 className="partName">ХОЛОДНІ НАПОЇ</h1>
         <Image
           src={pjecMenuIco}
-          width={90}
+          width={30}
           className="partHeadIco"
           alt="міні логотип теплого п'єца для меню "
         ></Image>
@@ -78,14 +87,11 @@ export default function ColdDrinks() {
       {coldDrinks.map((item) => {
         return (
           <div key={item.name} className="partItem">
-            <div>
-              <h2 className="itemName">{item.name}</h2>
-              <p className="itemDescrip">{item.description}</p>
-            </div>
-            <div className="itemPortionPrice">
-              <p>{item.portion}</p>
-              <p>{item.price}</p>
-            </div>
+            <img src={item.image} width={100}></img>
+            <h2 className="itemName">{item.name}</h2>
+            <p className="itemDescrip">{item.description}</p>
+            <p className="itemPortion">{item.portion}</p>
+            <p className="itemPrice">{item.price}</p>
           </div>
         );
       })}
